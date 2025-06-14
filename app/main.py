37efe0a4a03cb533c8e57eee3196ecfe19531baa
pvk-96 <praneethvarmakopperla@gmail.com>
2025-06-14 11:01:08 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,14 +1,11 @@
 import sys
-import os
 
-
-def raise_system_exit(exit_code):
-    raise SystemExit(exit_code)
-
-
-commands = {
-    "exit": lambda exit_code: os._exit(int(exit_code)),
+BUILTINS = {
+    "exit": lambda code=0, *_: sys.exit(int(code)),
     "echo": lambda *args: print(" ".join(args)),
+    "type": lambda cmd, *_: print(f"{cmd} is a shell builtin")
+    if cmd in BUILTINS
+    else print(f"{cmd}: not found"),
 }
 
 
@@ -17,16 +14,13 @@ def main():
         sys.stdout.write("$ ")
         sys.stdout.flush()
 
-        # Wait for user input
-        command_with_args = input().split()
-
-        command = command_with_args[0]
-
-        if not command in commands:
-            print(f"{command}: command not found")
-            continue
-
-        commands[command](*command_with_args[1:])
+        usr_input = input().split()
+        cmd = usr_input[0]
+        args = usr_input[1:]
+        if cmd in BUILTINS:
+            BUILTINS[cmd](*args)
+        else:
+            print(f"{cmd}: command not found")
 
 
 if __name__ == "__main__":
