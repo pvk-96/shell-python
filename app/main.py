96114cda45c4baaf06ba6aea4d7e1b3ce3589f00
pvk-96 <praneethvarmakopperla@gmail.com>
2025-06-14 10:53:32 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 import sys
 
+
 def main():
     while True:
         sys.stdout.write("$ ")
         command = input()
+        if command == "exit 0":
+            break
         print(f"{command}: command not found")
 
 
