30eca18bd20045d9a957e3df632ffe70df17d73d
pvk-96 <praneethvarmakopperla@gmail.com>
2025-06-14 11:08:20 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,69 +1,61 @@
 import sys
 import os
-
-commands = {}
-space = " "
-errors = []
-path = os.environ["PATH"]
+import subprocess
 
 
-def stopShell(args=["0"]):
-    sys.exit(int(args[0]))
-
-
-def echo(args):
-    print(space.join(args))
-
-
-def typeOf(args=["invalid"]):
-    checkType = space.join(args)
-    if checkType in commands:
-        print(f"{checkType} is a shell builtin")
-    elif findExe(args[0]):
-        exePath = findExe(args[0])
-        print(f"{args[0]} is {exePath}")
-    else:
-        print(f"{checkType}: not found")
-
-
-def findExe(exe):
-    paths = path.split(":")
-
-    for pathDir in paths:
-        try:
-            for filename in os.listdir(pathDir):
-                if filename == exe:
-                    filePath = os.path.join(pathDir, filename)
-                    if os.path.isfile(filePath) and os.access(filePath, os.X_OK):
-                        return filePath
-        except:
-            errors.append("bad path permissions")
-
+def find_in_path(command):
+    for path in os.environ.get("PATH", "").split(":"):
+        full_path = os.path.join(path, command)
+        if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+            return full_path
     return None
 
 
-def handleInput(inputs):
-    global commands
-    commands.update({"exit": stopShell, "echo": echo, "type": typeOf})
-    inputArr = inputs.split(" ")
+def type_command(*args):
+    global path
+    for cmd in args:
+        if cmd in commands:
+            print(f"{cmd} is a shell builtin")
+        else:
+            path = find_in_path(cmd)
+            if path:
+                print(f"{cmd} is {path}")
+            else:
+                print(f"{cmd}: not found")
 
-    if inputArr[0] in commands:
-        commands[inputArr[0]](inputArr[1:] if len(inputArr) > 1 else ["0"])
-    elif findExe(inputArr[0]):
-        os.system(inputs)
-    else:
-        print(f"{inputs}: command not found")
 
-    main()
+commands = {
+    "exit": lambda exit_code: os._exit(int(exit_code)),
+    "echo": lambda *args: print(" ".join(args)),
+    "type": type_command,
+    "pwd": lambda: print(os.getcwd()),
+}
 
 
 def main():
-    # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
+    try:
+        while True:
+            sys.stdout.write("$ ")
+            sys.stdout.flush()
+            command_with_args = input().split()
 
-    # Wait for user input
-    command = input()
-    handleInput(command)
+            command = command_with_args[0]
+            args = command_with_args[1:]
+
+            if command in commands:
+                commands[command](*args)
+            else:
+                path = find_in_path(command)
+                if path:
+                    try:
+                        subprocess.run([command] + args, executable=path)
+                    except Exception as e:
+                        print(f"Error executing {command}: {e}")
+                else:
+                    print(f"{command}: command not found")
+
+    except KeyboardInterrupt:
+        print("\nExiting...")
 
 
 if __name__ == "__main__":
