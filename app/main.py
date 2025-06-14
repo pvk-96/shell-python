84add76aa98f152be7ecae0ac6c4de6fad8eaeda
pvk-96 <praneethvarmakopperla@gmail.com>
2025-06-14 10:55:48 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,13 +1,32 @@
 import sys
+import os
+
+
+def raise_system_exit(exit_code):
+    raise SystemExit(exit_code)
+
+
+commands = {
+    "exit": lambda exit_code: os._exit(int(exit_code)),
+    "echo": lambda *args: print(" ".join(args)),
+}
 
 
 def main():
     while True:
         sys.stdout.write("$ ")
-        command = input()
-        if command == "exit 0":
-            break
-        print(f"{command}: command not found")
+        sys.stdout.flush()
+
+        # Wait for user input
+        command_with_args = input().split()
+
+        command = command_with_args[0]
+
+        if not command in commands:
+            print(f"{command}: command not found")
+            continue
+
+        commands[command](*command_with_args[1:])
 
 
 if __name__ == "__main__":
